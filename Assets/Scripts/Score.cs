9ee8de2ed18de8c5fd8866e9716cb61812dbf430
HIN16000832 <45455801+HindleyL@users.noreply.github.com>
2020-10-14 15:29:11 +0100

    Money

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public static int GlobalMoney;
+    public Text Money;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("addmoney", 2, 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void addmoney()
+    {
+        Money.text = GlobalMoney.ToString();
+    }
+}
